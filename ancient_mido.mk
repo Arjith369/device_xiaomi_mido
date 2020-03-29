e769600229abbd224246486dfe36072e44c71696
Smokey <kmboplays@gmail.com>
2020-03-30 00:24:59 +0500

    fix some stuff

--- ancient_mido.mk
+++ ancient_mido.mk
@@ -21,11 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common Ancient stuff.
-$(call inherit-product, vendor/ancient/config/common.mk)
+# Inherit some common Ancient ROM stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Gapps
 TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
 
 # Boot Animation res
 TARGET_BOOT_ANIMATION_RES := 1080
